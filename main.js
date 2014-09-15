759f83676831c762586568d0dd3dd6b633a6df8f
Simeg44 <Simeg44@gmail.com>
2014-09-15 12:00:06 -0600

    Final commit

--- main.js
+++ main.js
@@ -2,4 +2,14 @@ var word = prompt("Enter a word:");
 
 alert(word);
 
-alert(word.length);
+alert("There are " + word.length + " characters in this word.");
+
+alert("The third character is " + word.charAt(2));
+
+alert("Lowercase: " +word.toLowerCase());
+
+alert("Uppercase: " + word.toUpperCase());
+
+alert("My favorite word is " + word + "!");
+
+alert("Subword: " + word.charAt(1) + word.charAt(2) + word.charAt(3));
